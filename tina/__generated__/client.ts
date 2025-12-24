50e02bad51a7752cf50b5dcd341b968785d0e431
AdharshJolly <ajcodesofficial+github@gmail.com>
2025-12-25 00:45:46 +0530

    fix: Update client configuration to use production URL for GraphQL endpoint

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'b95ecae45b41243ea37bdedbeeb47510acf266a5', queries,  });
+export const client = createClient({ cacheDir: 'D:/Ongoing Projects/inc4-2026/tina/__generated__/.cache/1766603551132', url: 'https://content.tinajs.io/2.0/content/a35055d7-64c4-4928-ace0-edc5a7b4d2cb/github/development', token: 'b95ecae45b41243ea37bdedbeeb47510acf266a5', queries,  });
 export default client;
   
