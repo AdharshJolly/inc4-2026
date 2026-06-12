0704121ebf97a22c4866e94f534c49bb93000e20
AdharshJolly <ajcodesofficial+github@gmail.com>
2026-06-12 19:16:40 +0530

    feat: add PartnersSection component and partner images to showcase academic and industry partners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { HeroSection } from "@/components/home/HeroSection";
 import { Marquee } from "@/components/home/Marquee";
 import { AboutSection } from "@/components/home/AboutSection";
 import { CTASection } from "@/components/home/CTASection";
+import { PartnersSection } from "@/components/home/PartnersSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <Marquee />
       <AboutSection />
+      <PartnersSection />
       <CTASection />
     </main>
   );
